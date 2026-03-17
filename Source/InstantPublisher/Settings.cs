8df65ad137896e74a56e33085e67896ba8837d6a
la-lo-go <Gonzalo.alvalon@gmail.com>
2026-03-17 17:22:32 +0100

    feat: add Settings model for monitored items persistence
    Add MonitoredWebResource, MonitoredPlugin and MonitoredCollection
    classes to support saving/loading monitored items per connection.

---
+++ Source/InstantPublisher/Settings.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace Lalogo.InstantPublisher
+{
+    public class MonitoredWebResource
+    {
+        public string FilePath { get; set; }
+        public Guid WebResourceId { get; set; }
+        public string WebResourceName { get; set; }
+        public bool IsAuto { get; set; }
+    }
+
+    public class MonitoredPlugin
+    {
+        public string FilePath { get; set; }
+        public bool IsAuto { get; set; }
+    }
+
+    public class MonitoredCollection
+    {
+        public List<MonitoredWebResource> WebResources { get; set; } = new List<MonitoredWebResource>();
+        public List<MonitoredPlugin> Plugins { get; set; } = new List<MonitoredPlugin>();
+    }
+}
