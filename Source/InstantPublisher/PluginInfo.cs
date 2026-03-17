dea850a115e9e024272c501df36169329128bc22
la-lo-go <Gonzalo.alvalon@gmail.com>
2026-03-17 17:22:16 +0100

    refactor: remove Control property from PluginInfo and WebResourceInfo
    Replace UserControl references with flat state fields (FileName,
    IsDirty, IsBusy, IsAuto, LastPublish) to decouple data from UI.

--- Source/InstantPublisher/PluginInfo.cs
+++ Source/InstantPublisher/PluginInfo.cs
@@ -1,15 +1,16 @@
-﻿using System;
+using System;
 
 namespace Lalogo.InstantPublisher
 {
     public class PluginInfo
     {
-        public PluginViewControl Control { get; set; }
-
-        public DateTime LastPublish { get; set; } = DateTime.Now;
-
-
+        public string FileName { get; set; }
+        public bool IsAuto { get; set; }
+        public bool IsBusy { get; set; }
+        public bool IsDirty { get; set; }
+        public string AssemblyVersion { get; set; }
+        public string CrmVersion { get; set; }
+        public DateTime LastPublish { get; set; } = DateTime.MinValue;
         public Guid? PluginAssemblyId { get; set; }
-
     }
 }
--- Source/InstantPublisher/WebResourceInfo.cs
+++ Source/InstantPublisher/WebResourceInfo.cs
@@ -1,14 +1,15 @@
-﻿using System;
+using System;
 
 namespace Lalogo.InstantPublisher
 {
     public class WebResourceInfo
     {
-        public WebResourceViewControl Control { get; set; }
-
+        public string FileName { get; set; }
+        public string WebResourceName { get; set; }
         public Guid WebResourceId { get; set; }
-
-
-        public DateTime LastPublish { get; set; } = DateTime.Now;
+        public bool IsAuto { get; set; }
+        public bool IsBusy { get; set; }
+        public bool IsDirty { get; set; }
+        public DateTime LastPublish { get; set; } = DateTime.MinValue;
     }
 }
